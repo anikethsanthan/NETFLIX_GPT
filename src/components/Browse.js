26a7facaac5036d9dcae63d2191928830c76438e
anikethsanthan <anikethsanthan999@gmail.com>
2024-11-06 17:05:49 +0530

    fetched movie data made a custom hook to refactor my code and make it clean and dealt with some bugs

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,17 +1,11 @@
-import React, { useEffect } from 'react'
 import Header from './Header'
-import { API_Options } from '../utils/constants';
+import useNowPlayingMovies from '../Hooks/useNowPlayingMovies'
+
 
 const Browse = () => {
-  const getNowPlayingMovies=async()=>{
-    const data =await  fetch('https://api.themoviedb.org/3/movie/now_playing?page=1',API_Options);
-    const json=await data.json();
-    console.log(json);
-  }
 
-  useEffect(()=>{
-    getNowPlayingMovies();
-  },[]);
+  useNowPlayingMovies();
+  
   return (
     <div>
       <Header/>
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,9 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import  userReducer  from "react";
+import moviesReducer from "./movieSlice";
+
+//./moviesSlice
 
 const appStore=configureStore({
     reducer :{
-        user : userReducer,
+        users : userReducer,
+        movies: moviesReducer,
     }
 }
 
